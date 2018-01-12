55f46d3aecec46ed7439c038fced8577e7516bcd
Enno Hermann <enno.hermann@gmail.com>
2018-01-12 17:09:50 +0000

    Folder name workarounds no longer necessary.

--- egs/gp/s5/local/gp_prep_flists.sh
+++ egs/gp/s5/local/gp_prep_flists.sh
@@ -83,17 +83,7 @@ egrep 'XXX|TBA' $tmpdir/eval_spk \
 # We are going to use the 2-letter codes throughout, but the top-level 
 # directories of the GlobalPhone corpus use the full names of languages.
 full_name=`awk '/'$LCODE'/ {print $2}' $LANGMAP`;
-if [ $full_name == "Turkish" ]; then
-    full_name="Turkish/Turkish" # Turkish is nested in another subfolder
-elif [ $full_name == "Thai" ]; then
-    full_name="Thai/Thai" # Thai is nested in another subfolder
-elif [ $full_name == "Bulgarian" ]; then
-    full_name="Bulgarian/Bulgarian" # Bulgarian is nested in another subfolder
-elif [ $full_name == "Korean" ]; then
-    full_name="Korean/Korean" # Korean is nested in another subfolder
-elif [ $full_name == "Vietnamese" ]; then
-    full_name="Vietnamese/Vietnamese" # Vietnamese is nested in another subfolder
-elif [ $full_name == "Hausa" ]; then
+if [ $full_name == "Hausa" ]; then
     full_name="Hausa/Hausa/Data" # Hausa is nested in another subfolder
 fi
 ls "$GPDIR/$full_name/adc" | sed -e "s?^?$LCODE?" -e 's?$?_?' \
