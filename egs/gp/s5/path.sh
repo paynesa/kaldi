c923bb6893450d0d2d657dcf92085e5d7c0d035b
Sarah Brogden Payne <paynesa@sas.upenn.edu>
2020-01-09 17:10:41 -0500

    updated paths for CLIP

--- egs/gp/s5/path.sh
+++ egs/gp/s5/path.sh
@@ -3,7 +3,8 @@
 
 export LC_ALL=C  # For expected sorting and joining behaviour
 
-KALDI_ROOT=/group/project/cstr2/enno/kaldi
+#KALDI_ROOT=/group/project/cstr2/enno/kaldi
+KALDI_ROOT=/fs/clip-software/user-supported/kaldi
 [ -f $KALDI_ROOT/tools/env.sh ] && . $KALDI_ROOT/tools/env.sh
 
 [ ! -f $KALDI_ROOT/tools/config/common_path.sh ] && echo >&2 "The standard file $KALDI_ROOT/tools/config/common_path.sh is not present -> Exit!" && exit 1
@@ -12,19 +13,20 @@ KALDI_ROOT=/group/project/cstr2/enno/kaldi
 FSTBIN=$KALDI_ROOT/tools/openfst/bin
 LMBIN=$KALDI_ROOT/tools/irstlm/bin
 
-export PATH=$PWD/utils/:$PWD/local/:$FSTBIN:$LMBIN:$PWD:$PATH
+#export PATH=$PWD/utils/:$PWD/local/:$FSTBIN:$LMBIN:$PWD:$PATH
+export PATH=$PWD/utils/:$KALDI_ROOT/tools/openfst/bin:$PWD:$PATH
 
 # If the correct version of shorten and sox are not on the path,
 # the following will be set by local/gp_check_tools.sh
 SHORTEN_BIN=/group/project/cstr2/enno/zero/kaldi_globalphone/tools/shorten-3.6.1/bin
 # e.g. $PWD/tools/shorten-3.6.1/bin
-SOX_BIN=/group/project/cstr2/enno/zero/kaldi_globalphone/tools/sox-14.3.2/bin
+SOX_BIN=/opt/local/stow/sox-14.4.2/bin/sox
 # e.g. $PWD/tools/sox-14.3.2/bin
 
 export PATH=$PATH:$SHORTEN_BIN
 export PATH=$SOX_BIN:$PATH
 
 # Set the locations of the GlobalPhone corpus and language models
-export GP_CORPUS=/group/corporapublic/global_phone/
-export GP_LM=$DATA/gp_language_models/
-export ALIGN_DIR=$DATA/gp_alignments
+export GP_CORPUS=/fs/clip-realspeech/corpora/GP_tmp
+export GP_LM=$PWD/language_models
+export ALIGN_DIR=$PWD/language_models
